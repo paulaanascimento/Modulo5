e1346c606962198ca89705bc927950a34a3bec88
paula.nascimento <paula.nascimento@zup.com.br>
2023-06-14 16:27:54 -0300

    exercício 2

--- TestesJUnit/src/ex02/CalcularDiasSemanaTest.java
+++ TestesJUnit/src/ex02/CalcularDiasSemanaTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 public class CalcularDiasSemanaTest {
 
     @Test
-    public void deveRetornarODiaDaSemanaEDizerSeEOMelhorParaIrAFeira(){
+    public void deveRetornarODiaDaSemanaEFalse(){
         CalcularDiasSemana calcularDiasSemana = new CalcularDiasSemana();
         String resultado = calcularDiasSemana.diaDaSemana(4);
         Assertions.assertEquals("O dia é Quarta. É o melhor para ir à feira? false", resultado);
+    }
+
+    @Test
+    public void deveRetornarODiaDaSemanaETrue(){
+        CalcularDiasSemana calcularDiasSemana = new CalcularDiasSemana();
+        String resultado = calcularDiasSemana.diaDaSemana(1);
+        Assertions.assertEquals("O dia é Domingo. É o melhor para ir à feira? true", resultado);
     }
 }
